a14c697f4d52094235b89c2bcb45e376124fd386
Aaron Rodrigues <aaronrodrigues@Aarons-MBP.home>
2017-05-27 21:17:34 +0100

    Created unit tests for generating Paper and Scissors Stubbed out the behaviour of the random RPS generator!

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -4,9 +4,21 @@ describe Game do
   subject(:game) { described_class.new }
   
   let(:game_rock) { double :game, :result => 'Rock' }
+  let(:game_paper) { double :game, :result => 'Paper' }
+  let(:game_scissors) { double :game, :result => 'Scissors' }
 
   it 'generates rock paper scissor at random and returns Rock' do
     expect(game_rock.result).to eq('Rock') 
+  
+  end
+
+  it 'generates rock paper scissor at random and returns Rock' do
+    expect(game_paper.result).to eq('Paper') 
+  
+  end
+
+  it 'generates rock paper scissor at random and returns Rock' do
+    expect(game_scissors.result).to eq('Scissors') 
   
   end
 end
