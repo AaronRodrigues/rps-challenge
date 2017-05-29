879ddad843ba574ff84e80a945c677520dc54c25
Aaron Rodrigues <aaronrodrigues@Aarons-MBP.home>
2017-05-29 19:44:36 +0100

    Replaced Srand number with ROCK_SEED

--- spec/features/choose_weapon_spec.rb
+++ spec/features/choose_weapon_spec.rb
@@ -1,6 +1,7 @@
 # As a marketeer
 # So that I can enjoy myself away from the daily grind
 # I would like to be able to play rock/paper/scissors
+ROCK_SEED = 221561
 feature 'Choose weapon' do
 	 scenario 'from Rock, Paper or Scissor' do
  		 sign_in_and_play
@@ -12,7 +13,7 @@ end
 
 feature 'Player chooses Rock' do
   scenario 'computer chooses rock' do 
-    srand(221561)
+    srand(ROCK_SEED)
     sign_in_and_play
     click_button 'Rock'
     expect(page).to have_content 'Rock'
